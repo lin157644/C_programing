bac5cbe10aa47d9359aff9c674047626a4eb890a
lin157644 <linxs1010@gmail.com>
2021-01-14 05:41:34 +0800

    8_5

---
+++ Homework/8/8_5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int lines;
+	char c;
+	int freq[26]={0};
+	size_t ctr, ctr2;
+	
+	int* max=freq;
+	
+	scanf("%d", &lines);
+	getchar();
+	for(ctr=0; ctr<lines; ctr++)
+	{
+		while((c=getchar())!='\n')
+		{
+			if(isalpha(c)){
+				if(c>='a') freq[c-'a']++;
+				else freq[c-'A']++;
+				//puts("++ed");
+			}
+		}
+	}
+	
+	for(ctr=0; ctr<26; ctr++)
+	{
+		for(ctr2=0; ctr2<26; ctr2++)
+		{
+			if(freq[ctr2]>*max) {
+				max=&freq[ctr2];
+			}
+		}
+		if(*max!=0) {
+			printf("%c %d", max-freq+'A', *max);
+			*max = 0;
+			puts("");
+		} else {
+			break;
+		}
+		
+	}
+	
+
+} 
